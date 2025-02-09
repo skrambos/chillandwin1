a94a0f7e6bd78f1943137ec27c34646a39391c57
skrambos <kramberger.lovro@gmail.com>
2025-02-09 16:29:46 +0100

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -7,7 +7,7 @@ const NETWORKS = {
   DEVNET: {
     NETWORK: 'devnet',
     PROGRAM_ID: '3VBBRtUdiHK2QFXcaxjL4sLx26sTMJueaHVggoUzT8jf',    // Our lottery's brain address (smart contract) 🧠
-    LOTTERY_ADDRESS: 'HCV5EgASDtWV4nXkf6w7N4sidjiJnHCStwakjutHoubg', // The actual lottery - where the magic happens ✨
+    LOTTERY_ADDRESS: 'HhypjxEQYiVQZF5iJWwdUGrT9RkAatN7cU4BwbjVMW4h', // The actual lottery - where the magic happens ✨
     TREASURY_ADDRESS: 'DsVW3LGHi8FHRhy3yPzjX6WbgZvfFSZ7Dn4oKquHC8ok', // Treasury wallet for fees and stuff 💰
     RPC_ENDPOINT: 'https://api.devnet.solana.com',                    // How we talk to Solana (devnet edition) 🗣️
   },
